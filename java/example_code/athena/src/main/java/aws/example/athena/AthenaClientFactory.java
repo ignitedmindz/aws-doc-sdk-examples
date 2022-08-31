3afc03c9223a177a95b6e14989e2e41734725afa
ignitedmindz <104433396+ignitedmindz@users.noreply.github.com>
2022-08-31 15:43:25 +0530

    Update AthenaClientFactory.java
    changed from instance profile to default credential providers

--- java/example_code/athena/src/main/java/aws/example/athena/AthenaClientFactory.java
+++ java/example_code/athena/src/main/java/aws/example/athena/AthenaClientFactory.java
@@ -10,7 +10,7 @@
 package aws.example.athena;
 
 import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.athena.AmazonAthena;
 import com.amazonaws.services.athena.AmazonAthenaClientBuilder;
@@ -30,7 +30,7 @@ public class AthenaClientFactory
    */
   private final AmazonAthenaClientBuilder builder = AmazonAthenaClientBuilder.standard()
           .withRegion(Regions.US_WEST_2)
-          .withCredentials(InstanceProfileCredentialsProvider.getInstance())
+          .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
           .withClientConfiguration(new ClientConfiguration().withClientExecutionTimeout(ExampleConstants.CLIENT_EXECUTION_TIMEOUT));
 
   public AmazonAthena createClient()
